a142fdc0b8e86ecf7cb86d3494d26d0ca6001081
Elijah Kin <elijah.m.kin@gmail.com>
2024-09-06 17:48:48 -0400

    Serial matvec

--- matvec/src/matvec.s.c
+++ matvec/src/matvec.s.c
@@ -1,17 +1,19 @@
 #include <xmtc.h>
 
-int main(){
+int main() {
+  // WRITE YOUR CODE HERE
+  for (int row = 0; row < m; ++row) {
+    result[row] = 0;
+    for (int entry = rowptr[row]; entry < rowptr[row + 1]; ++entry) {
+      result[row] += values[entry] * vector[col_ind[entry]];
+    }
+  }
+  // END OF YOUR CODE
 
-	// WRITE YOUR CODE HERE	
-
-
-
-	// END OF YOUR CODE
-
-	// DONT MODIFY THE REST
-	#ifdef PRINT_RESULT
-	int i;
-	for(i=0;i<m;i++)
-		printf("%d\n",result[i]);
-	#endif
+// DONT MODIFY THE REST
+#ifdef PRINT_RESULT
+  for (int i = 0; i < m; ++i) {
+    printf("%d\n", result[i]);
+  }
+#endif
 }
