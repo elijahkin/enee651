45f5ed199d87c7641bae931870f467de08e6ba32
Elijah Kin <elijah.m.kin@gmail.com>
2024-09-06 19:13:04 -0400

    Parallel matvec

--- matvec/src/matvec.p.c
+++ matvec/src/matvec.p.c
@@ -1,17 +1,17 @@
 #include <xmtc.h>
 
-int main(){
+int main() {
+  spawn(0, m - 1) {
+    result[$] = 0;
+    for (int entry = rowptr[$]; entry < rowptr[$ + 1]; ++entry) {
+      result[$] += values[entry] * vector[col_ind[entry]];
+    }
+  }
 
-	// WRITE YOUR CODE HERE	
-
-
-
-	// END OF YOUR CODE
-
-	// DONT MODIFY THE REST
-	#ifdef PRINT_RESULT
-	int i;
-	for(i=0;i<m;i++)
-		printf("%d\n",result[i]);
-	#endif
+// DONT MODIFY THE REST
+#ifdef PRINT_RESULT
+  for (int i = 0; i < m; ++i) {
+    printf("%d\n", result[i]);
+  }
+#endif
 }
--- matvec/src/matvec.s.c
+++ matvec/src/matvec.s.c
@@ -1,14 +1,12 @@
 #include <xmtc.h>
 
 int main() {
-  // WRITE YOUR CODE HERE
   for (int row = 0; row < m; ++row) {
     result[row] = 0;
     for (int entry = rowptr[row]; entry < rowptr[row + 1]; ++entry) {
       result[row] += values[entry] * vector[col_ind[entry]];
     }
   }
-  // END OF YOUR CODE
 
 // DONT MODIFY THE REST
 #ifdef PRINT_RESULT
