fac32727c2bb3d12144a42c6618f370498e497ae
Elijah Kin <elijah.m.kin@gmail.com>
2024-09-06 22:26:59 -0400

    Updating verification script

--- matvec/src/matvec.sh
+++ matvec/src/matvec.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 
-# Testing correctness (no xlarge)
-xmtcc matvec.s.c -include ../data/small/matvec.h ../data/small/matvec.xbo -D PRINT_RESULT -quiet -o matvec.s
-xmtfpga matvec.s.b -o myFileSmall.txt
-diff -b myFileSmall.txt ../data/small/resultFileSmall.txt
+# TODO Exclude xlarge when testing correctness
+declare -a versions=("s" "p")
+declare -a sizes=("small" "medium" "large" "xlarge")
 
-xmtcc matvec.s.c -include ../data/medium/matvec.h ../data/medium/matvec.xbo -D PRINT_RESULT -quiet -o matvec.s
-xmtfpga matvec.s.b -o myFileMedium.txt
-diff -b myFileMedium.txt ../data/medium/resultFileMedium.txt
+## Loop over the sizes for both serial and parallel versions
+for version in "${versions[@]}"
+do
+  for size in "${sizes[@]}"
+  do
+    # First compile with printing to test correctness...
+    xmtcc matvec.$version.c -include ../data/$size/matvec.h ../data/$size/matvec.xbo -D PRINT_RESULT -quiet -o matvec.$version
+    xmtfpga matvec.$version.b -o myFile${size^}.txt > /dev/null
+    diff -b myFile${size^}.txt ../data/small/resultFile${size^}.txt
 
-xmtcc matvec.s.c -include ../data/large/matvec.h ../data/large/matvec.xbo -D PRINT_RESULT -quiet -o matvec.s
-xmtfpga matvec.s.b -o myFileLarge.txt
-diff -b myFileLarge.txt ../data/large/resultFileLarge.txt
-
-# Clock counting (printing disabled)
-xmtcc matvec.s.c -include ../data/small/matvec.h ../data/small/matvec.xbo -quiet -o matvec.s
-xmtfpga matvec.s.b
-
-xmtcc matvec.s.c -include ../data/medium/matvec.h ../data/medium/matvec.xbo -quiet -o matvec.s
-xmtfpga matvec.s.b
-
-xmtcc matvec.s.c -include ../data/large/matvec.h ../data/large/matvec.xbo -quiet -o matvec.s
-xmtfpga matvec.s.b
-
-xmtcc matvec.s.c -include ../data/xlarge/matvec.h ../data/xlarge/matvec.xbo -quiet -o matvec.s
-xmtfpga matvec.s.b
+    # ...then compile without printing to count clocks
+    xmtcc matvec.$version.c -include ../data/$size/matvec.h ../data/$size/matvec.xbo -quiet -o matvec.$version
+    xmtfpga matvec.$version.b -o myFile${size^}.txt
+  done
+done
