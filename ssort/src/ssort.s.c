1a9285e9d26a8bff9244c864c0c9d96d3e343f4c
Elijah Kin <elijah.m.kin@gmail.com>
2024-10-19 15:00:18 -0400

    Making heapify iterative

--- ssort/src/ssort.s.c
+++ ssort/src/ssort.s.c
@@ -1,27 +1,24 @@
-// #include <assert.h>
-// #include <stdio.h>
-
-// const int N = 6;
-// int A[] = {12, 11, 13, 5, 6, 7};
-// int result[N];
-
 void heapify(int A[], int n, int i) {
-  int largest = i;
-  int left = 2 * i + 1;
-  int right = 2 * i + 2;
+  while (true) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-  if (left < n && A[left] > A[largest]) {
-    largest = left;
-  }
-  if (right < n && A[right] > A[largest]) {
-    largest = right;
-  }
+    if (left < n && A[left] > A[largest]) {
+      largest = left;
+    }
+    if (right < n && A[right] > A[largest]) {
+      largest = right;
+    }
 
-  if (largest != i) {
+    if (largest == i) {
+      break;
+    }
+
     int temp = A[i];
     A[i] = A[largest];
     A[largest] = temp;
-    heapify(A, n, largest);
+    i = largest;
   }
 }
 
@@ -48,10 +45,5 @@ int main() {
 
   // Sort result
   heap_sort(result, N);
-
-  // Verify that result is correctly sorted
-  // for (int i = 1; i < N; ++i) {
-  //   assert(result[i] >= result[i - 1]);
-  // }
   return 0;
 }
